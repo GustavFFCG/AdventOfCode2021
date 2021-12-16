b018160bf7ab7ef21a3a0437888047384c6f32ed
GustavFFCG <gustav.ohman@ffcg.se>
2021-12-16 23:44:47 +0100

    Day 16 solved

--- Day16/PacketDecoder.fsx
+++ Day16/PacketDecoder.fsx
@@ -9,24 +9,42 @@ module Input =
         |> List.ofArray
         |> function | _::s::_ -> s | _ -> failwith "Please provide a filename for input"
 
-    let private toInput (strings: string seq) =
+    let toBinaryString s =
         let charToHex (c:char) =
             Int16.Parse(string c, NumberStyles.HexNumber)
         let intToBin (i:int16) =
             Convert.ToString(i,2).PadLeft(4,'0')
         let charToBin = charToHex >> intToBin
 
-        strings
-        |> Seq.head
-        |> Seq.fold (fun state c -> $"{state}{(charToBin c)}" ) ""
+        s |> Seq.fold (fun state c -> $"{state}{(charToBin c)}" ) ""
 
-    let binaryString = 
+    let fromParam = 
         File.ReadLines(fileName)
-        |> toInput
+        |> Seq.head
+        |> toBinaryString
+
+type OperatorType =
+    | Sum
+    | Product
+    | Minimum
+    | Maximum
+    | GreaterThan
+    | LessThan
+    | EqualTo
+module OperatorType =
+    let ofInt = function
+        | 0 -> Sum
+        | 1 -> Product
+        | 2 -> Minimum
+        | 3 -> Maximum
+        | 5 -> GreaterThan
+        | 6 -> LessThan
+        | 7 -> EqualTo
+        | i -> failwithf "invalid operatur type %i" i
 
 type PacketType =
-    | Literal of int
-    | Operator
+    | Literal of int64
+    | Operator of OperatorType
 type Packet = {
     Version: int
     Type: PacketType
@@ -34,67 +52,117 @@ type Packet = {
 }
 
 module Packet =
-    let private parseBinary s =
-        Convert.ToInt32(s, 2)
+    type ParseMode =
+        | Single
+        | Sequential
+        | FixNumber of int
 
-    let rec ofString (state:Packet list) (maxPackets:int option) (s:string): Packet list*string = 
+    let private parse32Binary s =
+        Convert.ToInt32(s, 2)
+    let private parse64Binary s =
+        Convert.ToInt64(s, 2)
+
+    let rec ofString (state:Packet list) parseMode (s:string): Packet list*string = 
         let rec parseLiteral (s:string) =
+            // printfn "parse literal %s" s
             let number, tail = s.Substring(1,4), s.Substring(5)
             if s.StartsWith("0") then
                 {| value=number; tail=tail |}
             else
-                {| value=(number + (parseLiteral tail).value); tail=tail |}
+                let nextLiteral = parseLiteral tail
+                {| value=(number + nextLiteral.value); tail=nextLiteral.tail |}
 
         let parseOperator (s:string) =
-            try
-                match s.Substring(0,1) with
-                    | "0" ->
-                        let length = s.Substring(1,15) |> parseBinary
-                        {|
-                            value= ofString [] None (s.Substring(16,length)) |> fst
-                            tail=s.Substring(16 + length)
-                        |}
-                    | "1" ->
-                        let nOfPackets = s.Substring(1,11) |> parseBinary
-                        let value, tail = s.Substring(12) |> ofString [] (Some nOfPackets)
-                        {|
-                            value=value
-                            tail=tail
-                        |}
-                    | _ -> failwith "illegal char"
-            with ex -> failwith $"exception parsing {s} as operator: {ex}" 
+            // printfn "parse operator %s" s
+            match s.Substring(0,1) with
+                | "0" ->
+                    let length = s.Substring(1,15) |> parse32Binary
+                    //printfn "Subpackets for %i bits" length
+                    if s.Length < (length + 1) then failwithf "Cannot draw %i from %s" length s
+                    let value, _tail = ofString [] Sequential (s.Substring(16,length))
+                    {|
+                        value=value
+                        tail=s.Substring(length + 16)
+                    |}
+                | "1" ->
+                    let nOfPackets = s.Substring(1,11) |> parse32Binary
+                    //printfn "Parse %i subpackets" nOfPackets
+                    let value, tail = s.Substring(12) |> ofString [] (FixNumber nOfPackets)
+                    {|
+                        value=value
+                        tail=tail
+                    |}
+                | _ -> failwith "illegal char"
 
-        if s.Length < 6 || not (Seq.exists (fun c -> c='1') s) then state, ""
-        else if maxPackets = Some 0 then state,s
+        if s.Length < 6 || not (Seq.exists (fun c -> c='1') s) then state, s
+        else if parseMode = FixNumber 0 then state,s
         else
-            let packetVersion = s.Substring(0, 3) |> parseBinary
+            let packetVersion = s.Substring(0, 3) |> parse32Binary
             let packetType, subPackets, tail = 
                 s.Substring(3, 3) 
-                |> parseBinary
+                |> parse32Binary
                 |> function
                     | 4 -> 
                         let literalContent = s.Substring(6) |> parseLiteral
                         literalContent.value
-                        |> parseBinary
+                        |> parse64Binary
                         |> Literal, [], literalContent.tail
-                    | _ -> 
+                    | n -> 
                         let operatorContent = s.Substring(6) |> parseOperator
-                        Operator, operatorContent.value , operatorContent.tail
+                        OperatorType.ofInt n |> Operator, operatorContent.value , operatorContent.tail
             let thisPacket = {
                 Version = packetVersion
                 Type = packetType
-                SubPackets = subPackets
+                SubPackets = subPackets |> List.rev
             }
             let newState = thisPacket :: state
-            let newMaxPackets = maxPackets |> Option.map (fun i -> i - 1)
-            ofString newState newMaxPackets tail
+            let newParsemode = 
+                parseMode |> function
+                    | FixNumber i -> FixNumber (i - 1)
+                    | other -> other
+            ofString newState newParsemode tail
 
     let rec sumVersionNumbers (packets: Packet list) =
         packets
         |> List.sumBy (fun p -> p.Version + (sumVersionNumbers p.SubPackets))
 
-Input.binaryString
-|> Packet.ofString [] None
-|> fst
+    let rec value (packet: Packet) =
+        match packet.Type with
+            | Literal i -> i
+            | Operator Sum          -> packet.SubPackets |> List.sumBy value
+            | Operator Product      -> packet.SubPackets |> List.fold (fun state p -> state * (value p)) 1L
+            | Operator Minimum      -> packet.SubPackets |> List.map value |> List.min
+            | Operator Maximum      -> packet.SubPackets |> List.map value |> List.max
+            | Operator GreaterThan  -> 
+                packet.SubPackets
+                |> function
+                    | [a;b] when (value a) > (value b) -> 1L
+                    | [a;b] -> 0L
+                    | _ -> failwith "exactly two packets required"
+            | Operator LessThan     ->
+                packet.SubPackets
+                |> function
+                    | [a;b] when (value a) < (value b) -> 1L
+                    | [a;b] -> 0L
+                    | _ -> failwith "exactly two packets required"
+            | Operator EqualTo      ->
+                packet.SubPackets
+                |> function
+                    | [a;b] when (value a) = (value b) -> 1L
+                    | [a;b] -> 0L
+                    | _ -> failwith "exactly two packets required"
+
+
+let packet = 
+    Input.fromParam
+    |> Packet.ofString [] Packet.ParseMode.Single
+    |> fst
+
+packet
 |> Packet.sumVersionNumbers
 |> printfn "Sum of versions: %i"
+
+packet
+|> List.head
+|> Packet.value
+|> printfn "Total value: %i"
