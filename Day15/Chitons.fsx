c1b323857a6e9a6a52c3ce90e61c1f901824d56a
GustavFFCG <gustav.ohman@ffcg.se>
2021-12-19 22:39:12 +0100

    Day 15 part 1

--- Day15/Chitons.fsx
+++ Day15/Chitons.fsx
@@ -5,27 +5,32 @@ open Microsoft.FSharp.Collections
 type Coord = {x: int; y:int}
     with static member ofxy x y = {x=x; y=y}
 
-type CoordinateRisk = {
-    Coord: Coord
-    Risk: int
-}
+type AggregateRisk =
+| Unknown
+| Preliminary of int
+| Final of int
+module AggregateRisk =
+    let isFinal = function | Final _ -> true |_ -> false
+    let finalRisk = function | Final r -> Some r | _ -> None
 
 let fileName = 
     fsi.CommandLineArgs 
     |> List.ofArray
     |> function | _::s::_ -> s | _ -> failwith "Please provide a filename for input"
 
-let toInput (strings: string seq) =
+let toInput strings =
     strings
-    |> Seq.map (fun s -> 
-        s |> Seq.map (string >> Int32.Parse)
-    ) |> array2D
+    |> Seq.mapi (fun y s -> 
+        s |> Seq.mapi (fun x c -> (Coord.ofxy x y), c |> string |> Int32.Parse |> (fun i -> i, if (x,y) = (0,0) then Preliminary 0 else Unknown))) 
+    |> Seq.concat
+    |> Map.ofSeq
+    |> fun m -> (strings |> Seq.length), m
 
 module CaveMap =
-    let array =
+    let size, nodes =
         File.ReadLines fileName
         |> toInput
-    let size = Array2D.length1 array
+    
     let adjacentCoordinates coord =
         [
             coord.x-1, coord.y
@@ -35,59 +40,47 @@ module CaveMap =
         |> List.map (fun (x,y) -> Coord.ofxy x y)
         |> List.filter (fun c -> 
             c.x >= 0 && c.y >= 0 && c.x < size && c.y < size)
+    let localRisk coord m = m |> Map.find coord |> fst
+    let aggregateRisk coord m = m |> Map.find coord |> snd
+    
+    let isComplete (m: Map<Coord,(int*AggregateRisk)>) = 
+        not(m |> Map.exists (fun c (_i, risk) -> not (AggregateRisk.isFinal risk) ))
+    
 
-let rec findPaths path coord target =
-    let pathHere = coord :: path
-    if coord = target then
-        Some [pathHere]
+let rec dijkstra  (cave: Map<Coord,(int*AggregateRisk)>) =
+    if CaveMap.isComplete cave then cave
     else
-        let candidatePoints = CaveMap.adjacentCoordinates coord
-        if List.contains target candidatePoints then 
-            Some [target :: pathHere]
-        else
-            let pointIsInPath c =
-                List.contains c path
-            let pointIsAdjacentToPath c =
-                CaveMap.adjacentCoordinates c
-                |> List.exists (fun c' -> List.contains c' path)
-            candidatePoints
-            |> List.filter (fun c -> not(pointIsInPath c || pointIsAdjacentToPath c))
-            |> function
-                | [] -> None
-                | l -> 
-                    l
-                    |> List.map (fun c -> findPaths pathHere c target)
-                    |> List.choose id
-                    |> List.concat
-                    |> Some
+        let minFinalNode = 
+            cave
+            |> Map.toSeq
+            |> Seq.map (fun (c, (i, risk)) -> 
+                match risk with
+                | Preliminary r -> Some (c,r) 
+                | _ -> None)
+            |> Seq.choose id
+            |> Seq.minBy (fun (_c, i) -> i)
+        
+        let newCave = 
+            CaveMap.adjacentCoordinates (fst minFinalNode)
+            |> List.filter (fun c -> 
+                cave |> CaveMap.aggregateRisk c |> AggregateRisk.isFinal |> not
+            )
+            |> List.fold (fun state c ->
+                let risk, aggregate = cave.Item c
+                let newAggregate = risk + (snd minFinalNode)
+                match aggregate with
+                    | Unknown -> 
+                        state |> Map.add c (risk, Preliminary newAggregate)
+                    | Preliminary r when r > newAggregate ->
+                        state |> Map.add c (risk, Preliminary newAggregate)
+                    | _ -> state
+                ) cave
+            |> Map.add (fst minFinalNode) (0, Final (snd minFinalNode))
+        dijkstra newCave
 
-let riskLevel (arr: int[,]) (path:Coord list) =
-    path
-    |> List.sumBy (fun coord -> arr.[coord.x,coord.y])
 
-let diagonal length i  =
-    seq{0..length - 1} |> List.ofSeq
-    |> List.map (fun x -> {x=x;y=2*i - x} )
-    |> List.filter (fun c -> c.x >= 0 && c.y >= 0 && c.x < length && c.y < length)
-
-let riskToDiagonal i (riskToPrevDiagonal:CoordinateRisk list): CoordinateRisk list =
-    let risk coord =
-        riskToPrevDiagonal
-        |> List.map (fun risk ->
-            findPaths [] risk.Coord coord
-            |> Option.map (List.map (fun path -> 
-                    risk.Risk + riskLevel CaveMap.array path))
-            |> Option.map List.min
-        )
-        |> List.choose id
-        |> List.min
-
-    diagonal CaveMap.size i
-    |> List.map (fun c -> { Coord=c; Risk = risk c } )
-
-let diagonalZero = [ {Coord={x=0; y=0}; Risk = 0 } ]
-
-seq{1..CaveMap.size - 1}
-|> Seq.fold (fun state i -> riskToDiagonal i state) diagonalZero
-|> Seq.head
-|> fun risk -> printfn "Risk is %i" risk.Risk
+let caveMap = CaveMap.nodes
+dijkstra caveMap
+|> Map.find (Coord.ofxy (CaveMap.size - 1) (CaveMap.size - 1))
+|> snd
+|> printfn "Risk is %A"
